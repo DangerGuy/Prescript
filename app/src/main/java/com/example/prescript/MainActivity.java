97c2b578f4a76b0754695980665f004eecad4662
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-01-21 15:08:37 -0800

    Added working button and authenticator and password stuff

--- app/src/main/java/com/example/prescript/MainActivity.java
+++ app/src/main/java/com/example/prescript/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.prescript;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.editTextTextEmailAddress);
+        TextView password = (TextView) findViewById(R.id.editTextTextPassword);
+
+        Button btn = (Button) findViewById(R.id.button);
+
+        //
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(username.getText().toString().equals("admin@gmail.com") && password.getText().toString().equals("admin@gmail.com")) {
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                }else
+                    Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
