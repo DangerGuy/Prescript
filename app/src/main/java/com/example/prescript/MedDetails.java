509a815f1f259db1a70377864d304793bedca5ff
Lucas Qin <lq2020@students.cs.ubc.ca>
2023-01-21 21:48:53 -0800

    finished functionality for medication details

--- app/src/main/java/com/example/prescript/MedDetails.java
+++ app/src/main/java/com/example/prescript/MedDetails.java
@@ -2,7 +2,9 @@ package com.example.prescript;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MedDetails extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MedDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_details);
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra(UserInterface.MEDICATION_NAME);
+        TextView nameTV = (TextView) findViewById(R.id.medicationName);
+        nameTV.setText(name);
+
+        String isP = intent.getBooleanExtra(UserInterface.MEDICATION_IS_P, false) ?
+                "This medication is prescribed." : "This medication is over the counter.";
+        TextView pTV = (TextView) findViewById(R.id.medicationIsP);
+        pTV.setText(isP);
+
+        String desc = intent.getStringExtra(UserInterface.MEDICATION_DESC);
+        TextView descTV = (TextView) findViewById(R.id.medicationDesc);
+        descTV.setText(desc);
     }
 }
