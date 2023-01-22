cfd19cf866742a9c96db3bbc5eb8ce3f0995a93c
Frederick Wang <79602963+DangerGuy@users.noreply.github.com>
2023-01-21 17:46:25 -0800

    Added functionality to the user name be variable

--- app/src/main/java/com/example/prescript/MainActivity.java
+++ app/src/main/java/com/example/prescript/MainActivity.java
@@ -11,12 +11,12 @@ import android.widget.Toast;
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_TEXT = "com.example.prescript.example.EXTRA_TEXT";
     private static Map<String, String> initializeLogins() {
         Map<String, String> lg = new HashMap<>();
 
         // place logins, as pairs of usernames and passwords, below
-        lg.put("admin@gmail.com", "admin@gmail.com");
+        lg.put("a", "a");
 
         return lg;
     }
@@ -33,22 +33,21 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.button);
 
-        //
-
         btn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 if(logins.containsKey(username.getText().toString())
                         && logins.get(username.getText().toString()).equals(password.getText().toString())) {
                     Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                    openUI();
+                    openUI("abc");
                 }else
                     Toast.makeText(MainActivity.this, "LOGIN FAILED", Toast.LENGTH_SHORT).show();
             }
         });
     }
-    public void openUI(){
+    public void openUI(String name){
         Intent intent = new Intent(this, userInterface.class);
+        intent.putExtra(EXTRA_TEXT, name);
         startActivity(intent);
     }
 
--- app/src/main/java/com/example/prescript/userInterface.java
+++ app/src/main/java/com/example/prescript/userInterface.java
@@ -2,7 +2,9 @@ package com.example.prescript;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class userInterface extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class userInterface extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_interface);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(MainActivity.EXTRA_TEXT);
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText(text);
     }
 }
